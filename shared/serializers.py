178ffdc1972e6232ae376d56f0eabad0ea9570fc
bakatrouble <bakatrouble@gmail.com>
2017-03-11 14:19:10 +0300

    Add person to client model (update serializer)

--- shared/serializers.py
+++ shared/serializers.py
@@ -7,4 +7,4 @@ class ClientSerializer(serializers.ModelSerializer):
     class Meta:
         model = Client
         fields = ('id', 'full_name', 'short_name', 'registration_address', 'location_address', 'phone', 'inn', 'kpp',
-                  'account_number', 'bank', 'bik', 'ogrn',)
+                  'account_number', 'bank', 'bik', 'ogrn', 'person_name', 'person_post',)

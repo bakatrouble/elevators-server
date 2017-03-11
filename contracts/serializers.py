252d445a7460e0a3b1433aa53e95066dad26ac84
bakatrouble <bakatrouble@gmail.com>
2017-03-11 17:12:50 +0300

    create/edit contracts finished

--- contracts/serializers.py
+++ contracts/serializers.py
@@ -1,15 +1,12 @@
 from rest_framework import serializers
 
-from applications.serializers import ApplicationSerializer
 from .models import Contract
-from shared.models import Client
 
 
 class ContractSerializer(serializers.ModelSerializer):
-    client = serializers.PrimaryKeyRelatedField(source='application.client', queryset=Client.objects.all())
-    application_data = ApplicationSerializer(source='application', read_only=True)
+    client = serializers.PrimaryKeyRelatedField(source='application.client', read_only=True)
 
     class Meta:
         model = Contract
-        fields = ('id', 'application', 'application_data', 'number', 'date', 'finish_date', 'price', 'terms', 'poa',
+        fields = ('id', 'application', 'number', 'date', 'finish_date', 'price', 'terms', 'poa',
                   'poa_number', 'poa_date', 'client')
